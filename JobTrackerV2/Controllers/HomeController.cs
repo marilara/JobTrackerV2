df01684057f43157f57e0a138d90a0cf195d2e6e
maricela <laram@mail.gvsu.edu>
2020-01-18 01:30:36 -0500

    add files & solution

---
+++ JobTrackerV2/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using JobTrackerV2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobTrackerV2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IStudentRepository _studentRepository;
+
+        public HomeController(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+        public string Index()
+        {
+            return _studentRepository.GetStudent(1).Name;
+        }
+    }
+}
---
+++ JobTrackerV2/Models/MockStudentRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobTrackerV2.Models
+{
+    public class MockStudentRepository : IStudentRepository
+    {
+        private List<Student> _studentList;
+
+        public MockStudentRepository()
+        {
+            _studentList = new List<Student> ()
+            {
+                new Student() { Id = 1, Name = "Mary", Course = "Back-end", Email = "mary@gmail.com" },
+                new Student() { Id = 2, Name = "John", Course = "Back-end", Email = "john@gmail.com" },
+                new Student() { Id = 3, Name = "Sarah", Course = "Back-end", Email = "sarah@gmail.com" }
+            };
+        }
+
+        public Student GetStudent(int Id)
+        {
+            return _studentList.FirstOrDefault(s => s.Id == Id);
+        }
+    }
+}
